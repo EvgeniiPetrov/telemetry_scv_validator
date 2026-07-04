dad8c173dd1c9903941e34a0cd18136bf56f2340
Evgenii Petrov <epoy74@gmail.com>
2026-07-04 19:38:56 +1000

    feat: implement validate_stantion_id

---
+++ src/telemetry_csv_validator/validators.py
@@ -0,0 +1,11 @@
+﻿# uv pip install -e .
+def validate_station_id(station_id: object) -> bool:
+
+    if not isinstance(station_id, str):
+        return False
+    clean_station_id = station_id.strip()
+    if not clean_station_id:
+        return False
+    return True
+
+# print(validate_station_id(None))
